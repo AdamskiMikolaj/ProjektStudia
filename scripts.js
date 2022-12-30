ed71e022231636b8c290829a4a50193c433ffc67
AdamskiMikolaj <102051176+AdamskiMikolaj@users.noreply.github.com>
2022-12-30 04:41:10 +0100

    Update scripts.js
    dodanie skryptu dla content4

--- scripts.js
+++ scripts.js
@@ -25,3 +25,38 @@ menubutton.onclick = function() {
 		licznik = 0;
 	}
 }
+
+// skrypt content4
+
+var odmierzCzas = setInterval(timer, 1750);
+
+function timer() {
+	zmienObraz();
+}
+
+var czas = 0;
+var producencilista = ["razer.svg","steelseries.svg","logitech.svg","amd.svg","intel.svg","nvidia.svg","kingston.svg","coolermaster.svg","microsoft.svg","adata.svg"];
+var numerChoice;
+var numerUsun;
+
+function zmienObraz() {
+	if (czas < 10) {
+		document.getElementById("logoarea").style.visibility = "visible";
+		document.getElementById("choice9").style.backgroundColor = "#1e1e1e";
+		document.getElementById("choice9").style.boxShadow = "none";
+		var numerId = document.getElementById("choice"+czas).id;
+		numerChoice = numerId.charAt(6);
+		numerUsun = numerChoice - 1;
+		document.getElementById("choice"+czas).style.backgroundColor = "#353535";
+		document.getElementById("choice"+czas).style.boxShadow = "0px 0px 10px #000000";
+		if (numerUsun > -1) {
+			document.getElementById("choice"+numerUsun).style.backgroundColor = "#1e1e1e";
+			document.getElementById("choice"+numerUsun).style.boxShadow = "none";
+		}
+		document.getElementById("logoareaimage").setAttribute("src", "grafika/producenci/"+producencilista[czas]);
+		document.getElementById("choice"+czas);
+		czas++;
+	} else {
+		czas = 0;
+	}
+}
