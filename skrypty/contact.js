b3c61f346b358cb9230b3b4c09b504b49594a927
AdamskiMikolaj <102051176+AdamskiMikolaj@users.noreply.github.com>
2023-01-07 04:46:17 +0100

    Add files via upload
    skrypty do podstrony kontakt

---
+++ skrypty/contact.js
@@ -0,0 +1,28 @@
+function changeFacebook() {
+    document.getElementById("contactsocialfirst").style.display = "none";
+    document.getElementById("contactsocialfacebook").style.display = "block";
+    document.getElementById("contactsocialtwitter").style.display = "none";
+    document.getElementById("contactsocialinstagram").style.display = "none";
+    document.getElementById("contactsocialtiktok").style.display = "none";
+}
+function changeTwitter() {
+    document.getElementById("contactsocialfirst").style.display = "none";
+    document.getElementById("contactsocialfacebook").style.display = "none";
+    document.getElementById("contactsocialtwitter").style.display = "block";
+    document.getElementById("contactsocialinstagram").style.display = "none";
+    document.getElementById("contactsocialtiktok").style.display = "none";
+}
+function changeInstagram() {
+    document.getElementById("contactsocialfirst").style.display = "none";
+    document.getElementById("contactsocialfacebook").style.display = "none";
+    document.getElementById("contactsocialtwitter").style.display = "none";
+    document.getElementById("contactsocialinstagram").style.display = "block";
+    document.getElementById("contactsocialtiktok").style.display = "none";
+}
+function changeTiktok() {
+    document.getElementById("contactsocialfirst").style.display = "none";
+    document.getElementById("contactsocialfacebook").style.display = "none";
+    document.getElementById("contactsocialtwitter").style.display = "none";
+    document.getElementById("contactsocialinstagram").style.display = "none";
+    document.getElementById("contactsocialtiktok").style.display = "block";
+}
