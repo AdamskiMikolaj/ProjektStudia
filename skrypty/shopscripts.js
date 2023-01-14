f9b3db39ea85f9869bf101378f2d8309536a5fdc
AdamskiMikolaj <102051176+AdamskiMikolaj@users.noreply.github.com>
2023-01-14 17:08:11 +0100

    Add files via upload
    js sklepu

---
+++ skrypty/shopscripts.js
@@ -0,0 +1,6 @@
+//ukryj sekcję wybór kategorii, pokaż sekcję wyświetlania produktów sklepu
+
+function chooseCategory() {
+    document.getElementById("choosearea").style.display = "none";
+    document.getElementById("shoparea").style.display = "block";
+}
