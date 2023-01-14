7514b172c32310901ab272201bc320eb980fba2d
AdamskiMikolaj <102051176+AdamskiMikolaj@users.noreply.github.com>
2023-01-14 21:18:40 +0100

    Delete shopscripts.js
    update shop

--- skrypty/shopscripts.js
+++
@@ -1,6 +0,0 @@
-//ukryj sekcję wybór kategorii, pokaż sekcję wyświetlania produktów sklepu
-
-function chooseCategory() {
-    document.getElementById("choosearea").style.display = "none";
-    document.getElementById("shoparea").style.display = "block";
-}
